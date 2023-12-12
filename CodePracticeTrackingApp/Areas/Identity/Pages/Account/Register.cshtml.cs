f54a6a48076a3792b5365c861a0d525521cfaed4
Thomas Ngo <tngo0508@gmail.com>
2023-12-12 15:21:25 -0800

    finish the home page

--- CodePracticeTrackingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ CodePracticeTrackingApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -138,7 +138,8 @@ namespace CodePracticeTrackingApp.Areas.Identity.Pages.Account
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
         {
-            returnUrl ??= Url.Content("~/");
+            //returnUrl ??= Url.Content("~/");
+            returnUrl ??= Url.Content("~/Problem");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
